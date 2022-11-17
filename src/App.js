1f09b66176dd756d4acc5cef6dcc99c72285d76b
mehedi-hasan22 <mehedi30554@gmail.com>
2022-11-18 03:01:54 +0600

    added routes & header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blogs from './Components/Blogs/Blogs';
+import Header from './Components/Header/Header';
+import Main from './Components/Layout/Main';
+import Statistics from './Components/Statistics/Statistics';
+import Topics from './Components/Topics/Topics';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [{
+        path: '/topics',
+        element: <Topics></Topics>
+      },
+      {
+        path: '/statistics',
+        element: <Statistics></Statistics>
+      },
+      {
+        path: '/blog',
+        element: <Blogs></Blogs>
+      },
+      {
+        path: '*',
+        element: <div>
+          <h1>oppss!!</h1>
+          <h3>Route isn't defined. try again</h3>
+          <h6>Error Code:404</h6>
+        </div>
+      }],
+    }
+
+  ])
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light navbar-dark bg-primary">
+            <div className="container container-fluid text-white">
+                <a className="navbar-brand fs-1 m-3" href="/#">DevQwiz</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav fs-5 ms-auto mb-2 mb-lg-0 text-white">
+                        <li className="nav-item text-white">
+                            <Link className='nav-link active' to='/'>Home</Link>
+                        </li>
+                        <li className="nav-item text-white">
+                            <Link className='nav-link active' to='/topics'>Topics</Link>
+                        </li>
+                        <li className="nav-item text-white">
+                            <Link className='nav-link active' to='/statistics'>Statistics</Link>
+                        </li>
+                        <li className="nav-item text-white">
+                            <Link className='nav-link active' to='/about'>About</Link>
+                        </li>
+                        <li className="nav-item text-white">
+                            <Link className='nav-link active' to='/blog'>Blog</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
