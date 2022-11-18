628391611e6a34e6fb3da9ff7e1b8e4441c41b2c
mehedi-hasan22 <mehedi30554@gmail.com>
2022-11-18 21:48:43 +0600

    added reminder

--- src/Components/TopicsDetails/TopicsDetails.js
+++ src/Components/TopicsDetails/TopicsDetails.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import './TopicDetails.css'
 
 const TopicsDetails = () => {
     const topics = useLoaderData().data.questions;
     // const topicsQuestion = useLoaderData().data.question;
-    // console.log(topics)
+
+    const handleClick = (op, correctAnswer) => {
+        // console.log('answer clicked', op, correctAnswer);
+        if (op === correctAnswer) {
+            document.getElementById('wr-ans').classList.remove('d-none')
+            document.getElementById('r8-ans').classList.add('d-none')
+        }
+        else {
+            document.getElementById('wr-ans').classList.add('d-none')
+            document.getElementById('r8-ans').classList.remove('d-none')
+        }
+
+    };
+
+    const showAns = () => {
+        console.log('show kormu???')
+    }
+    console.log(topics)
     return (
         <div>
+            <div>
+                <h2 id='wr-ans' className='border mt-2 text-info d-none'>Write Answer!!!</h2>
+                <h2 id='r8-ans' className='border mt-2 text-danger d-none'>Wrong Answer!!!</h2>
+            </div>
             {
-                topics.map(qq => <section className='questions container mt-5 border rounded p-5' key={qq.question}>
-                    <h3>{qq.question}</h3>
+                topics.map(qq => <section className='questions container mt-5 border rounded p-5 buttons' key={qq.question}>
+                    <h5>{qq.question}</h5>
+                    <p id='show' className='d-none'>{qq.correctAnswer}</p>
                     {
-                        qq.options.map(op => <h5 key={op} className='mt-3 border rounded p-3'><input className="form-check-input mt-2" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                            <label className="form-check-label" htmlFor="flexRadioDefault2">
-                                {op}
-                            </label></h5>)
+                        qq.options.map(op => <button onClick={() => handleClick(op, qq.correctAnswer)} key={op} id='answer-btn' className='mt-3 border rounded p-3 button'>
+                            {op}
+                        </button>)
                     }
                     <button className='btn btn-primary px-5 py-2 mt-2'>Show Answer</button>
                 </section>)
+
             }
         </div >
     );
